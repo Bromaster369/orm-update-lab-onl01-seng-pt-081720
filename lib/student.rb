510800b5e13dc05b0db4581b1ce6ead897ce286f
Christian Pardo <chrispardopersonal@gmail.com>
2020-10-27 21:16:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,10 +37,7 @@ class Student
     DB[:conn].execute(sql)
   end 
   
-  def update
-    sql = "UPDATE students SET name = ?, grade = ?, WHERE id = ?"
-    DB[:conn].execute(sql, self.name, self.grade, self.id)
-  end 
+
   
   def save 
     if self.id
@@ -55,5 +52,10 @@ class Student
     @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")[0][0]
    end 
   end  
+  
+  # def update
+  #   sql = "UPDATE students SET name = ?, grade = ?, WHERE id = ?"
+  #   DB[:conn].execute(sql, self.name, self.grade, self.id)
+  # end 
   
 end
